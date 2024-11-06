f11cab52f023445cb83232fcc0ccba87e1004f23
Dahye <psst5491@naver.com>
2024-11-06 19:50:21 +0900

    ✨ feat:  route 수정 및 작가용 메뉴 추가

--- src/components/BNB/index.tsx
+++ src/components/BNB/index.tsx
@@ -3,13 +3,23 @@
 import { usePathname } from 'next/navigation';
 import { Container, Tab } from './styles';
 
+// 고객 뷰일 경우
 const MENU_LIST = [
-  { title: '홈', route: '/' },
   { title: '사진작가', route: '/photographer' },
   { title: '포토스팟', route: '/map' },
-  { title: '예약', route: '/reservation' },
-  { title: '마이페이지', route: '/profile' },
+  { title: '홈', route: '/' },
+  { title: '예약', route: '/book' },
+  { title: '마이페이지', route: '/my' },
 ];
+
+// [todo] 이후 고객/작가 구분하게 될 때 활성화하기
+// const MENU_LIST = [
+//   { title: '사진작가', route: '/photographer' },
+//   { title: '포토스팟', route: '/map' },
+//   { title: '홈', route: '/' },
+//   { title: '예약', route: '/book' },
+//   { title: '마이페이지', route: '/profile' },
+// ];
 
 export default function BottomNavigationBar() {
   const pathname = usePathname();
