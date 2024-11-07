eb3a8790694c59ffb79d878ee657e40712b39d43
Dahye <psst5491@naver.com>
2024-11-07 11:46:28 +0900

    🐛 fix: marker 오류 수정

--- src/app/map/components/Marker/index.tsx
+++ src/app/map/components/Marker/index.tsx
@@ -3,31 +3,38 @@ import { useLayoutEffect, useRef } from 'react';
 import { Position } from '@/constants/map';
 import { useNaverMapContext } from '@/contexts/naverMapContext';
 import { useNaverMapsContext } from '@/contexts/naverMapsContext';
-import { MARKER_HEIGHT, MARKER_WIDTH, MarkerItem } from './style';
+import { MARKER_HEIGHT, MARKER_WIDTH } from './style';
 
 export default function Marker({ position }: { position: Position }) {
   const naverMaps = useNaverMapsContext();
   const mapContext = useNaverMapContext();
-  const markerRef = useRef(null);
 
   useLayoutEffect(() => {
-    if (!markerRef.current || !mapContext) {
-      return;
-    }
+    if (!mapContext) return;
 
-    // [error] 네이버 예제에서는 이렇게 하던데(TS가 아니라 JS)
-    // no-new 규칙을 지킬 방법을 못찾겠어요
-    // eslint-disable-next-line no-new
-    new naverMaps.Marker({
+    // MarkerItem을 스타일을 가진 HTMLDivElement로 생성
+    const markerItem = document.createElement('div');
+    markerItem.style.width = `${MARKER_WIDTH}px`;
+    markerItem.style.height = `${MARKER_HEIGHT}px`;
+    markerItem.style.border = '2px solid red';
+    markerItem.style.borderRadius = '100%';
+
+    // 네이버 맵 Marker 생성
+    const marker = new naverMaps.Marker({
       position: new naverMaps.LatLng(position.lat, position.lng),
       map: mapContext.map,
       icon: {
-        content: markerRef.current,
+        content: markerItem,
         size: new naverMaps.Size(MARKER_WIDTH, MARKER_HEIGHT),
         anchor: new naverMaps.Point(MARKER_WIDTH / 2, MARKER_HEIGHT),
       },
     });
+
+    // 컴포넌트 언마운트 시 Marker 제거
+    return () => {
+      marker.setMap(null);
+    };
   }, [mapContext, naverMaps, position]);
 
-  return <MarkerItem ref={markerRef} />;
+  return null; // MarkerItem을 직접 렌더링하지 않음
 }
