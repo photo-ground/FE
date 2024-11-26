f59348bfc2dbda48cf5c49a2229792381277d1f6
Dahye <psst5491@naver.com>
2024-11-26 14:27:59 +0900

    💄 ui: background blur 추가

--- src/components/BNB/styles.ts
+++ src/components/BNB/styles.ts
@@ -10,6 +10,7 @@ export const Container = styled.nav`
   padding: 1rem 1.25rem;
   background: #0e0e0ecc;
   border-top: 1px solid ${({ theme }) => theme.colors.gray[800]};
+  backdrop-filter: blur(25px);
 `;
 
 export const Tab = styled(Link)`
