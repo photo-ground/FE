73ee231b9ec985e49aebeac0c5e3c2135ebb4f2e
Dahye <psst5491@naver.com>
2024-11-26 12:15:23 +0900

    💄 ui: bnb 아이콘 및 색상 적용

--- src/components/BNB/styles.ts
+++ src/components/BNB/styles.ts
@@ -1,15 +1,27 @@
 import Link from 'next/link';
 import styled from 'styled-components';
+import Text from '../atoms/Text';
 
 export const Container = styled.nav`
   display: flex;
   align-items: center;
   justify-content: space-between;
 
-  height: 3rem; // [todo] remove this
-  background: #ccc; // [todo] remove this
+  padding: 1rem 1.25rem;
+  background: #0e0e0ecc;
+  border-top: 1px solid ${({ theme }) => theme.colors.gray[800]};
 `;
 
-export const Tab = styled(Link)<{ $isSelected: boolean }>`
-  color: ${(props) => (props.$isSelected ? 'blue' : 'black')};
+export const Tab = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.25rem;
+
+  text-decoration: none;
+`;
+
+export const TabText = styled(Text)<{ $isSelected: boolean }>`
+  color: ${({ theme, $isSelected }) =>
+    $isSelected ? theme.colors.primary[100] : theme.colors.gray[300]};
 `;
