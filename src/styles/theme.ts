0fb73158ca3acd171dd45169728e2382c06f46b7
Dahye <psst5491@naver.com>
2024-11-26 09:28:16 +0900

    🎨 style: 디자인 시스템 색상 정의

---
+++ src/styles/theme.ts
@@ -0,0 +1,36 @@
+const theme = {
+  colors: {
+    primary: {
+      100: '#FF4000',
+      80: '#FF612B',
+      70: '#FF855C',
+      60: '#FFA67D',
+    },
+
+    secondary: {
+      surface: '#007C29',
+      alpha: '#007C2947',
+    },
+
+    black: '#0E0E0E',
+    white: '#F5F5F5',
+
+    gray: {
+      900: '#212121',
+      800: '#262626',
+      700: '#333333',
+      600: '#404040',
+      500: '#595959',
+      400: '#737373',
+      300: '#8C8C8C',
+      200: '#A6A6A6',
+      100: '#BFBFBF',
+    },
+
+    system: {
+      warning: '#ED4245',
+    },
+  },
+};
+
+export default theme;
