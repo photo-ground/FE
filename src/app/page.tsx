16574a8ec1376d215a7990bd1d2857f6186798df
Dahye <psst5491@naver.com>
2024-11-27 20:30:56 +0900

    🔥 remove: 예시 코드 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,7 @@
 'use client';
 
-import CTAButton from '@/components/atoms/CTAButton';
-import MediumButton from '@/components/atoms/MediumButton';
-import SmallButton from '@/components/atoms/SmallButton';
-import Tag from '@/components/atoms/Tag';
-import TNB from '@/components/TNB';
 import React from 'react';
 
 export default function Home() {
-  return (
-    <div>
-      <TNB.Main />
-      <TNB.Title text="예약관리" />
-      <TNB.SubTitle text="예약관리" />
-      <TNB.Back text="예약관리" />
-
-      <SmallButton.Primary text="팔로우" />
-      <SmallButton.Primary text="팔로우" disabled />
-      <SmallButton.Secondary text="팔로우" />
-      <SmallButton.Tertiary text="팔로우" />
-
-      <Tag text="세련된" variant="primary" />
-      <Tag text="세련된" variant="secondary" />
-
-      <MediumButton text="스냅 전체보기" variant="primary" />
-      <MediumButton text="스냅 전체보기" variant="secondary" />
-
-      <CTAButton text="예약하기" variant="primary" />
-      <CTAButton text="예약하기" variant="secondary" />
-      <CTAButton text="예약하기" variant="primary" disabled />
-      <CTAButton text="예약하기" variant="primary" disabled />
-    </div>
-  );
+  return <div />;
 }
