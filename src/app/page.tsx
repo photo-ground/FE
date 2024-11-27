ad0e653f819529a48c58fb7e0bda730e74117563
Dahye <psst5491@naver.com>
2024-11-27 18:22:31 +0900

    💄 ui: Tag 컴포넌트 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Button from '@/components/atoms/Button';
+import Tag from '@/components/atoms/Tag';
 import React from 'react';
 
 export default function Home() {
@@ -10,6 +11,9 @@ export default function Home() {
       <Button.Small.Primary text="팔로우" disabled />
       <Button.Small.Secondary text="팔로우" />
       <Button.Small.Tertiary text="팔로우" />
+
+      <Tag text="세련된" variant="primary" />
+      <Tag text="세련된" variant="secondary" />
     </div>
   );
 }
