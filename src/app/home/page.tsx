629ba0a8ce0f064fe9c0ab2db80fec208732570b
hiwon-lee <84234411+hiwon-lee@users.noreply.github.com>
2024-12-09 00:27:51 +0900

    ✨ feat:  Filter컴퍼넌트 적용

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,14 +1,16 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 
 import Banner from '@/components/Banner';
 import Spacer from '@/components/Spacer';
 import Card from '@/components/Card';
 import TNB from '@/components/TNB';
+import Text from '@/components/atoms/Text';
 import styled from 'styled-components';
 import SearchEngine from './_components/SearchEngine';
-import HeaderTitle from './_components/HeaderTitle';
+import Filter from './_components/Filter';
+import { Option, UNIV_LIST, UnivLabel, UnivValue } from './type/Option';
 
 const Container = styled.div`
   position: relative;
@@ -44,7 +46,16 @@ const CardContainerY = styled.div`
   margin: 0 auto;
 `;
 
+const TitleContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 1.25rem;
+  margin-top: 0;
+`;
+
 export default function HomePage() {
+  const [univ, setUniv] = useState<UnivValue | null>('yonsei');
+  const [univTitle, setUnivTitle] = useState<UnivLabel | null>('연세대학교');
   // 10개의 카드 데이터를 생성
   const cards = Array.from({ length: 10 }, (_, index) => ({
     id: index,
@@ -53,13 +64,20 @@ export default function HomePage() {
     src: 'https://via.placeholder.com/150',
     title: `Small Card ${index + 1}`,
   }));
+  const onChangeUniv = (prop: Option) => {
+    setUniv(prop.value);
+    setUnivTitle(prop.label);
+  };
   return (
     <Container>
       <Background src="/images/background1.webp" alt="background" />
       <TNB.Main />
       <SearchEngine />
-      <Spacer size="24px" />
-      <HeaderTitle title="활발히 활동 중인 작가" />
+      <Spacer size="1.5rem" />
+      <TitleContainer>
+        <Text variant="title1_sb">활발히 활동 중인 작가</Text>
+        <Text variant="caption1_rg"> 학교변경</Text>
+      </TitleContainer>
       <CardContainerX>
         {cards.map((card) => (
           <Card
@@ -71,12 +89,19 @@ export default function HomePage() {
           />
         ))}
       </CardContainerX>
-      <Spacer size="48px" />
+      <Spacer size="3rem" />
 
       <Banner src="https://via.placeholder.com/150" />
-      <Spacer size="48px" />
-
-      <HeaderTitle title="최근 연세대학교 스냅 사진" />
+      <Spacer size="3rem" />
+      <TitleContainer>
+        <Text variant="title1_sb">최근 {univTitle} 스냅 사진</Text>
+        <Filter
+          optionList={UNIV_LIST}
+          placeholder="더보기"
+          value={univ}
+          onChange={onChangeUniv}
+        />
+      </TitleContainer>
 
       <CardContainerY>
         {' '}
