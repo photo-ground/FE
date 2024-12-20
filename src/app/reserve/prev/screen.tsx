d7be95d22493a29b08fcc79943da838f1c1cd94d
Dahye <psst5491@naver.com>
2024-12-20 16:34:24 +0900

    💄 ui: 이전 예약 페이지

---
+++ src/app/reserve/prev/screen.tsx
@@ -0,0 +1,112 @@
+'use client';
+
+import LocationIcon from '@/assets/LocationIcon';
+import PeopleIcon from '@/assets/PeopleIcon';
+import PhotographerIcon from '@/assets/PhotographerIcon';
+import Chip from '@/components/atoms/Chip';
+import LargeButton from '@/components/atoms/LargeButton';
+import Text from '@/components/atoms/Text';
+import TNB from '@/components/TNB';
+import styled from 'styled-components';
+
+const Container = styled.ol``;
+
+const CardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+  padding: 1.5rem 1.25rem;
+`;
+
+const CardContainer = styled.div`
+  display: flex;
+  gap: 2rem;
+`;
+
+const Profile = styled.img`
+  width: 3rem;
+  height: 3rem;
+  border-radius: 100%;
+`;
+
+const TextArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+  width: 100%;
+`;
+
+const Content = styled.div`
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+  gap: 0.625rem;
+`;
+
+const InfoArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.25rem;
+`;
+
+const InfoLine = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.25rem;
+`;
+
+const InfoText = styled(Text)`
+  color: ${({ theme }) => theme.colors.gray[200]};
+`;
+
+const DivideLine = styled.hr`
+  width: 100%;
+  border: none;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.gray[700]};
+`;
+
+function Card() {
+  return (
+    <CardWrapper>
+      <CardContainer>
+        <Profile src="/images/yonsei.jpg" />
+        <TextArea>
+          <Text variant="body1_md">11.03(일) 오후 3:00</Text>
+
+          <Content>
+            <InfoArea>
+              <InfoLine>
+                <PhotographerIcon />
+                <InfoText variant="body1_md">이채린 작가</InfoText>
+              </InfoLine>
+              <InfoLine>
+                <LocationIcon />
+                <InfoText variant="body1_md">홍익대학교</InfoText>
+              </InfoLine>
+            </InfoArea>
+
+            <Chip icon={PeopleIcon} text="2인" />
+          </Content>
+        </TextArea>
+      </CardContainer>
+
+      <LargeButton text="후기 작성" variant="secondary" />
+    </CardWrapper>
+  );
+}
+
+export default function PrevReservationScreen() {
+  return (
+    <>
+      <TNB.Back text="이전 스냅" />
+
+      <Container>
+        <Card />
+        <DivideLine />
+        <Card />
+        <DivideLine />
+        <Card />
+      </Container>
+    </>
+  );
+}
