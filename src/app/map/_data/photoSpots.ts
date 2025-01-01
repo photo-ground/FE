8b348cd83d4164b0e4ae1100073bf11baa128216
hiwon-lee <84234411+hiwon-lee@users.noreply.github.com>
2025-01-01 17:16:12 +0900

    📝 docs:  더미 데이터 생성성

---
+++ src/app/map/_data/photoSpots.ts
@@ -0,0 +1,27 @@
+const photoSpots = [
+  {
+    lat: 37.5511,
+    lng: 126.9407,
+    title: '포토스팟 1',
+    src: '/images/ewha.jpg',
+  },
+  {
+    lat: 37.5647,
+    lng: 126.9386,
+    title: '포토스팟 2',
+    src: '/images/yonsei.jpg',
+  },
+  {
+    lat: 37.5617,
+    lng: 126.9468,
+    title: '포토스팟 3',
+    src: '/images/sogang.jpg',
+  },
+  {
+    lat: 37.5519,
+    lng: 126.9246,
+    title: '포토스팟 4',
+    src: '/images/ewha.jpg',
+  },
+];
+export default photoSpots;
