7431a5d91999913413d444a687d170e2d6a1f82b
hiwon-lee <84234411+hiwon-lee@users.noreply.github.com>
2025-01-18 14:43:45 +0900

    💡comment:   delete

--- src/app/photographerProfile/_services/getActivePhotographer.ts
+++ src/app/photographerProfile/_services/getActivePhotographer.ts
@@ -11,7 +11,6 @@ export async function getActivePhotographer(): Promise<PhotographerProps> {
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/photographer/active`,
   );
 
-  console.log(res.ok);
   if (!res.ok) {
     const errorResponse = await res.json();
     throw new Error(errorResponse.message || '문제가 발생했습니다.');
@@ -32,7 +31,6 @@ export async function getUnivSpotList(
       },
     },
   );
-  console.log(res);
   if (!res.ok) {
     const errorResponse = await res.json();
     throw new Error(errorResponse.message || '문제가 발생했습니다.');
@@ -45,10 +43,8 @@ export async function postNewContent(
   photographerId: number,
   newContent: PostUploadContainerProps,
 ) {
+  // 데이터를 FormData로 전송해야한다.
   const formData = new FormData();
-
-  // JSON 데이터를 FormData에 추가
-  // formData.append('jsonData', JSON.stringify(newContent.postInfo));
 
   // JSON 데이터를 Blob으로 추가
   const postInfoBlob = new Blob([JSON.stringify(newContent.postInfo)], {
@@ -57,26 +53,15 @@ export async function postNewContent(
   formData.append('postInfo', postInfoBlob);
 
   // 사진 파일 추가
-  newContent.photos.forEach((photo, index) => {
+  newContent.photos.forEach((photo) => {
     formData.append(`photos`, photo);
   });
-  // FormData 데이터 확인
-  // for (const [key, value] of formData.entries()) {
-  //   if (value instanceof Blob) {
-  //     console.log(`Key: ${key}, Value: [Blob], Type: ${value.type}`);
-  //   } else {
-  //     console.log(`Key: ${key}, Value: ${value}`);
-  //   }
-  // }
+
   // 기본 헤더 구성
   const getHeaders = (token: string) => ({
     Authorization: `${token}`,
     'Content-Type': 'multipart/form-data',
   });
-
-  // const getHeaders = () => ({
-  //   'Content-Type': 'multipart/form-data',
-  // });
 
   // 요청할 주소
   const url = `${process.env.NEXT_PUBLIC_BASE_URL}/api/posts/${photographerId}`;
@@ -84,14 +69,11 @@ export async function postNewContent(
   try {
     // 1. Access Token 가져오기
     const accessToken = localStorage.getItem('accessToken');
-    console.log(accessToken);
-    console.log(formData);
 
     // 2. 첫 번째 요청 시도
     const response = await axios.post(url, formData, {
       headers: getHeaders(accessToken || ''),
     });
-    console.log(response);
 
     return response.data;
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -99,7 +81,6 @@ export async function postNewContent(
     // 3. 401 오류 발생 시 토큰 갱신
     if (error.response?.status === 401) {
       const newAccessToken = await refreshAccessToken();
-      console.log(newAccessToken);
       localStorage.setItem('accessToken', newAccessToken); // 갱신된 토큰 저장
 
       // 4. 갱신된 토큰으로 재요청
@@ -107,7 +88,6 @@ export async function postNewContent(
         headers: getHeaders(newAccessToken),
       });
 
-      console.log(retryResponse);
       return retryResponse.data;
     }
 
