2a0f5843da38b29e8d35cc38222abeb1a6d9b50c
hiwon-lee <84234411+hiwon-lee@users.noreply.github.com>
2025-01-23 17:03:26 +0900

    ✨ feat:  로그인 여부에 따른 페이지 전환 기능

--- src/app/my/editinfo/page.tsx
+++ src/app/my/editinfo/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { AccountSection, DivideLine } from '@/app/signup/styles';
 import CTAButton from '@/components/atoms/CTAButton';
@@ -16,6 +16,13 @@ import {
   UnivInput,
 } from '@/app/my/_component';
 import RightChevronIcon from '@/assets/RightChevronIcon';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import {
+  deleteUser,
+  getUserInfo,
+  updateUserInfo,
+} from '../_services/getUserInfo';
+import { UpdateUserInfoProps, UserInfoProps } from '@/types/user';
 /* eslint-disable jsx-a11y/label-has-associated-control */
 const ButtonWrapper = styled.div`
   padding: 0 1.25rem;
@@ -35,58 +42,105 @@ const LeaveButton = styled.div`
 export default function EditProfile() {
   // const router = useRouter();
   // const { query } = router;
-  const userInfo = {
-    id: 1,
-    name: '문서영',
-    email: 'fhflwhwl5@naver.com',
-    phone: '010-1234-5678',
-    gender: 'FEMALE',
-    univ: '이화여자대학교',
-  };
 
-  const updateInfo = async () => {
-    // const response = await signup({ data: signUpData });
-    // if (response) {
-    //   router.push('signin');
-    // }
-  };
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [userData, setUserData] = useState<UpdateUserInfoProps>({
+    name: '',
+    phone: '',
+    myUniv: '',
+    gender: '',
+  });
+
+  const { data: userInfo } = useQuery<UserInfoProps>({
+    queryKey: ['userInfo'],
+    queryFn: getUserInfo,
+  });
+
+  if (userInfo) {
+    userData.gender = userInfo.gender;
+    userData.myUniv = userInfo.univ;
+    userData.name = userInfo.name;
+    userData.phone = userInfo.phone;
+  }
+
+  const deleteUserMutation = useMutation({
+    mutationFn: deleteUser,
+    onSuccess: (data) => {
+      console.log(data);
+      setModalOpen(true);
+    },
+  });
+
+  const updateUserMutation = useMutation({
+    mutationKey: ['updateUserInfo'],
+    mutationFn: updateUserInfo,
+    onSuccess: (data) => {
+      console.log(data);
+      setModalOpen(true);
+    },
+  });
+  // const userInfo = {
+  //   id: 1,
+  //   name: '문서영',
+  //   email: 'fhflwhwl5@naver.com',
+  //   phone: '010-1234-5678',
+  //   gender: 'FEMALE',
+  //   univ: '이화여자대학교',
+  // };
 
   const handleUpdateInfo = () => {
-    console.log('update!');
+    updateUserMutation.mutate(userData);
+  };
+
+  const handleLeaveButton = () => {
+    deleteUserMutation.mutate();
   };
   return (
     <>
-      <Container>
-        <TNB.Back text="내 정보 수정" />
-        <AccountSection>
-          <Text variant="title3">계정 정보</Text>
-          <EmailInput value={userInfo.email} onChange={updateInfo} />
-          <PasswordUpdate value="888888!a" />
-        </AccountSection>
-        <DivideLine />
+      {userInfo && (
+        <Container>
+          <TNB.Back text="내 정보 수정" />
+          <AccountSection>
+            <Text variant="title3">계정 정보</Text>
+            <EmailInput value={userInfo.email} />
+            <PasswordUpdate value="888888!a" />
+          </AccountSection>
+          <DivideLine />
 
-        <UserSection>
-          <Text variant="title3">회원 정보</Text>
+          <UserSection>
+            <Text variant="title3">회원 정보</Text>
 
-          <NameInput value={userInfo.name} onChange={updateInfo} />
-          <PhoneInput value={userInfo.phone} onChange={updateInfo} />
-          <UnivInput value={userInfo.univ} onChange={updateInfo} />
-          <GenderInput value={userInfo.gender} onChange={updateInfo} />
-        </UserSection>
-        <DivideLine />
-        <LeaveButton>
-          <Text variant="body2_rg" color="#8C8C8C">
-            탈퇴하기
-          </Text>
-          <RightChevronIcon size="24px" color="#8C8C8C" />
-        </LeaveButton>
-        <Spacer size="32px" />
+            <NameInput
+              value={userInfo.name}
+              onChange={(value) => setUserData({ ...userData, name: value })}
+            />
+            <PhoneInput
+              value={userInfo.phone}
+              onChange={(value) => setUserData({ ...userData, phone: value })}
+            />
+            <UnivInput
+              value={userInfo.univ}
+              onChange={(value) => setUserData({ ...userData, myUniv: value })}
+            />
+            <GenderInput
+              value={userInfo.gender}
+              onChange={(value) => setUserData({ ...userData, gender: value })}
+            />
+          </UserSection>
+          <DivideLine />
+          <LeaveButton onClick={handleLeaveButton}>
+            <Text variant="body2_rg" color="#8C8C8C">
+              탈퇴하기
+            </Text>
+            <RightChevronIcon size="24px" color="#8C8C8C" />
+          </LeaveButton>
+          <Spacer size="32px" />
 
-        <ButtonWrapper onClick={handleUpdateInfo}>
-          <CTAButton text="수정완료" onClick={updateInfo} />
-        </ButtonWrapper>
-      </Container>
-
+          <ButtonWrapper onClick={handleUpdateInfo}>
+            <CTAButton text="수정완료" />
+          </ButtonWrapper>
+        </Container>
+      )}
       <Spacer size="32px" />
     </>
   );
---
+++ src/app/my/layout.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { redirect } from 'next/navigation';
+import { isUserAuthenticated } from '@/lib/authentication';
+
+export default function ProtectedLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function checkAuth() {
+      try {
+        // Call the server-side function to check authentication
+        const authResult = await isUserAuthenticated();
+
+        setIsAuthenticated(authResult);
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Authentication check failed', error);
+        setIsAuthenticated(false);
+        setIsLoading(false);
+      }
+    }
+
+    checkAuth();
+  }, []);
+
+  // Show loading state
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  // Redirect if not authenticated
+  if (!isAuthenticated) {
+    redirect('/signin');
+  }
+
+  return <>{children}</>;
+}
