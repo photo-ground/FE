2bf69166918903945b2fc2b732c6a9b2b7ef243d
hiwon-lee <84234411+hiwon-lee@users.noreply.github.com>
2025-01-23 14:31:22 +0900

    ✨ feat:  api function

--- src/app/my/_services/getUserInfo.ts
+++ src/app/my/_services/getUserInfo.ts
@@ -12,7 +12,7 @@ const getHeaders = (token: string) => ({
 // my : 고객 정보 조회
 export async function getUserInfo() {
   // 요청할 주소
-  const url = `${process.env.NEXT_PUBLIC_BASE_URL}/api/customer}`;
+  const url = `${process.env.NEXT_PUBLIC_BASE_URL}/api/customer`;
 
   try {
     // 1. Access Token 가져오기
@@ -53,7 +53,7 @@ export async function updateUserInfo({
   gender,
 }: UpdateUserInfoProps) {
   // 요청할 주소
-  const url = `${process.env.NEXT_PUBLIC_BASE_URL}/api/customer}`;
+  const url = `${process.env.NEXT_PUBLIC_BASE_URL}/api/customer`;
 
   const userUpdateInfo = JSON.stringify({
     name,
@@ -102,10 +102,10 @@ export async function updateUserInfo({
   }
 }
 
-// my : 고객 비밀번호 변경경
+// my : 고객 비밀번호 변경
 export async function updateUserPassword(password: string) {
   // 요청할 주소
-  const url = `${process.env.NEXT_PUBLIC_BASE_URL}/api/customer}`;
+  const url = `${process.env.NEXT_PUBLIC_BASE_URL}/api/customer`;
 
   // 데이터를 FormData로 전송해야한다.
   const passwordInfo = JSON.stringify({
@@ -144,6 +144,51 @@ export async function updateUserPassword(password: string) {
       }
 
       return '회원정보 수정에 실패했습니다.';
+    }
+
+    // 5. 기타 오류 처리
+    console.error('Error uploading content:', error);
+    throw error;
+  }
+}
+
+// my : 고객 회원 탈퇴퇴
+export async function deleteUser() {
+  // 요청할 주소
+  const url = `${process.env.NEXT_PUBLIC_BASE_URL}/api/customer/delete`;
+
+  try {
+    // 1. Access Token 가져오기
+    const accessToken = localStorage.getItem('accessToken');
+
+    // 2. 첫 번째 요청 시도
+    const response = await axios.patch(url, {
+      headers: getHeaders(accessToken || ''),
+    });
+
+    if (response.status === 200) {
+      return response;
+    }
+
+    return '회원정보 수정에 실패했습니다.';
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    // 3. 401 오류 발생 시 토큰 갱신
+    if (error.response?.status === 401) {
+      const newAccessToken = await refreshAccessToken();
+      localStorage.setItem('accessToken', newAccessToken); // 갱신된 토큰 저장
+
+      // 4. 갱신된 토큰으로 재요청
+      const retryResponse = await axios.patch(url, {
+        headers: getHeaders(newAccessToken),
+      });
+
+      if (retryResponse.status === 200) {
+        return retryResponse;
+      }
+
+      return '회원 탈퇴에 실패했습니다.';
     }
 
     // 5. 기타 오류 처리
