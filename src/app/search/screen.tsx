78799f748a9024eb96a4b25c513829cbf2532ce6
Dahye <psst5491@naver.com>
2025-01-24 14:51:01 +0900

    ✨ feat: 작가 검색 페이지

---
+++ src/app/search/screen.tsx
@@ -0,0 +1,186 @@
+'use client';
+
+import Link from 'next/link';
+import styled from 'styled-components';
+
+import SearchIcon from '@/assets/SearchIcon';
+import Text from '@/components/atoms/Text';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import CancelIcon from '@/assets/CancelIcon';
+import useDebounce from '@/lib/useDebounce';
+import getSearchResult from './_libs/getSearchResult';
+import { PhotographerSummary } from '../photographer/_libs/getPhotographerList';
+
+const Wrapper = styled.div`
+  position: relative;
+  height: 100dvh;
+
+  -ms-overflow-style: none;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+const Background = styled.img`
+  position: absolute;
+  width: 100%;
+  object-fit: cover;
+  z-index: -1;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+
+  padding: 1.25rem;
+`;
+
+const SearchArea = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+
+  width: 100%;
+`;
+
+const InputContainer = styled.div<{ $isTyping?: boolean }>`
+  flex: 1;
+  display: flex;
+  gap: 0.5rem; // 아이콘과 입력창 사이 간격을 임의로 설정
+
+  padding: 0.688rem 1.25rem;
+  padding-right: ${({ $isTyping }) => ($isTyping ? '0.625rem' : '1.25rem')};
+
+  //이거머임?
+  background: linear-gradient(
+    90deg,
+    rgba(140, 140, 140, 0.2) 0%,
+    rgba(89, 89, 89, 0.2) 100%
+  );
+  border: 1px solid rgba(191, 191, 191, 0.2);
+  border-radius: 1.5rem;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  background: transparent;
+  border: none;
+  outline: none;
+
+  font-size: ${({ theme }) => theme.typography.body2_rg.fontSize};
+  font-weight: ${({ theme }) => theme.typography.body2_rg.fontWeight};
+  line-height: ${({ theme }) => theme.typography.body2_rg.lineHeight};
+  color: ${({ theme }) => theme.colors.white};
+`;
+
+const ButtonWrapper = styled.button`
+  all: unset;
+
+  background: transparent;
+  border: none;
+  outline: none;
+  cursor: pointer;
+`;
+
+const CancelButton = styled(ButtonWrapper)`
+  height: 1.25rem;
+`;
+
+const PhotographerList = styled.ol`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+const PhotographerItem = styled(Link)`
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+
+  background: transparent;
+  border: none;
+  outline: none;
+
+  text-decoration: none;
+
+  cursor: pointer;
+`;
+
+const Profile = styled.img`
+  width: 2.625rem;
+  height: 2.625rem;
+  border-radius: 100%;
+`;
+
+export default function SearchScreen() {
+  const [query, setQuery] = useState('');
+  const [result, setResult] = useState<PhotographerSummary[]>([]);
+  const router = useRouter();
+
+  useDebounce(
+    () => {
+      getSearchResult({ name: query }).then((response) => {
+        setResult(response.photographerList);
+      });
+    },
+    [query],
+    500,
+  );
+
+  const goBack = () => {
+    router.back();
+  };
+
+  return (
+    <Wrapper>
+      <Background src="/images/background1.webp" alt="background" />
+
+      <Container>
+        <SearchArea>
+          <InputContainer $isTyping={!!query}>
+            <Input
+              placeholder="작가 검색하기"
+              value={query}
+              onChange={(event) => {
+                setQuery(event.target.value);
+              }}
+            />
+            {query ? (
+              <CancelButton
+                onClick={() => {
+                  setQuery('');
+                }}
+              >
+                <CancelIcon />
+              </CancelButton>
+            ) : (
+              <SearchIcon size="1.5rem" />
+            )}
+          </InputContainer>
+
+          <ButtonWrapper onClick={goBack}>
+            <Text variant="title2_md">취소</Text>
+          </ButtonWrapper>
+        </SearchArea>
+
+        <PhotographerList>
+          {result.map((photographer) => (
+            <li key={photographer.photographerId}>
+              <PhotographerItem
+                href={`/photographer/${photographer.photographerId}`}
+              >
+                <Profile
+                  src={photographer.profileUrl}
+                  alt={photographer.photographerName}
+                />
+                <Text variant="body1_md">{photographer.photographerName}</Text>
+              </PhotographerItem>
+            </li>
+          ))}
+        </PhotographerList>
+      </Container>
+    </Wrapper>
+  );
+}
---
+++ src/lib/useDebounce.ts
@@ -0,0 +1,15 @@
+import { useEffect, useCallback } from 'react';
+
+export default function useDebounce(
+  effect: () => void,
+  dependencies: string[],
+  delay: number,
+) {
+  // eslint-disable-next-line
+  const callback = useCallback(effect, dependencies);
+
+  useEffect(() => {
+    const timeout = setTimeout(callback, delay);
+    return () => clearTimeout(timeout);
+  }, [callback, delay]);
+}
