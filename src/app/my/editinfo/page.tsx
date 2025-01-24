c08af2e16edf1a8fa30671ac4b4a05397657cc8c
hiwon-lee <84234411+hiwon-lee@users.noreply.github.com>
2025-01-25 00:46:29 +0900

    🐛 fix:  dropout 403 error

--- src/app/my/editinfo/page.tsx
+++ src/app/my/editinfo/page.tsx
@@ -78,7 +78,7 @@ export default function EditProfile() {
     },
   });
 
-  // 회원 정보 업데이트 함수수
+  // 회원 정보 업데이트 함수
   const updateUserMutation = useMutation({
     mutationKey: ['updateUserInfo'],
     mutationFn: updateUserInfo,
