c208f9c418cbb14b4a7ca96057f4b0b7ffe45ae6
hiwon-lee <84234411+hiwon-lee@users.noreply.github.com>
2025-01-28 01:38:17 +0900

    Revert "🚑 hotfix:  얕은 방지 코드만"
    This reverts commit c885991a5ef3b08f278f9f136bd0f0179b6abf61.

--- src/app/photographerProfile/[id]/writePost/page.tsx
+++ src/app/photographerProfile/[id]/writePost/page.tsx
@@ -5,10 +5,6 @@ export default async function PhotographerWritePostPage({
 }: {
   params: Promise<{ id: number }>;
 }) {
-  if (!params) {
-    alert('접근할 수 없습니다.');
-    // Router.
-  }
   const id = (await params).id!;
 
   return <Main photographerId={id} />;
