1141d0a60fe346dac0b4bffad2db6a3075edcb96
hiwon-lee <84234411+hiwon-lee@users.noreply.github.com>
2025-01-27 16:41:32 +0900

    ♻️ refactor: modal swiper -  use swiper module

--- src/app/map/_components/Modal.tsx
+++ src/app/map/_components/Modal.tsx
@@ -8,8 +8,43 @@ import styled from 'styled-components';
 import { IconButton } from '@mui/material';
 import CloseIcon from '@/assets/CloseIcon';
 
-import Slider, { SliderData } from './Slider';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Scrollbar, A11y } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+import Text from '@/components/atoms/Text';
+import SmallButton from '@/components/atoms/SmallButton';
+import { useRouter } from 'next/navigation';
+
+import { SliderData } from './Slider';
+
 import useSpotStore from '../_store';
+
+const ImageContainer = styled.div`
+  position: relative;
+  width: 100%;
+  max-width: 286px;
+  aspect-ratio: 3 / 4;
+  text-align: center;
+  overflow: hidden;
+  background-color: ${({ theme }) => theme.colors.black};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 86px;
+  margin: 0 auto;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+  object-fit: contain;
+`;
 
 const ModalContainer = styled.div`
   position: absolute;
@@ -39,9 +74,23 @@ const CloseHeader = styled.div`
 `;
 const ContentWrapper = styled.div`
   // flex: 1; /* 남은 공간을 채움 */
+  // margin: 0 auto;
   display: flex;
   align-items: center; /* 세로 기준으로 가운데 정렬 */
   justify-content: center; /* 가로 기준으로 가운데 정렬 */
+`;
+
+const Info = styled.div`
+  margin-top: 24px;
+  margin-bottom: 16px;
+  text-align: center;
+`;
+
+const SwiperSlideBox = styled(SwiperSlide)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 0;
 `;
 
 interface ModalProps {
@@ -52,14 +101,20 @@ interface ModalProps {
 
 // photoSpot배열에서 currIndex를 먼저 찾아 보여주고 그 기준으로 좌우왔다갔다
 export default function Modal({ sliderData, setModalState }: ModalProps) {
+  const router = useRouter();
   const clearCurrPostIdIndex = useSpotStore(
     (state) => state.clearCurrPostIdIndex,
   );
+
   const currPostIdIndex = useSpotStore((state) => state.currPostIdIndex);
 
   const handleModalClose = () => {
     setModalState(false);
     clearCurrPostIdIndex();
+  };
+
+  const handleSmallButton = (postId: number) => {
+    router.push(`/post/${postId}`);
   };
 
   return ReactDOM.createPortal(
@@ -72,10 +127,39 @@ export default function Modal({ sliderData, setModalState }: ModalProps) {
           </IconButton>
         </CloseHeader>
         <ContentWrapper>
-          <Slider
+          <Swiper
+            // install Swiper modules
+            modules={[Navigation, Scrollbar, A11y]}
+            spaceBetween={50}
+            slidesPerView={1}
+            navigation
+            initialSlide={currPostIdIndex as number}
+            scrollbar={{ draggable: true }}
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log('slide change')}
+          >
+            {sliderData.map((item) => (
+              <SwiperSlideBox>
+                <ImageContainer>
+                  <Image src={item.imageUrl} alt={item.imageUrl} />
+                </ImageContainer>
+                <Info>
+                  <Text variant="header2">{item.photographerName}</Text>
+                  <Text variant="body3" color="#a6a6a6">
+                    {item.univ} | {item.spotName}
+                  </Text>
+                </Info>
+                <SmallButton.Tertiary
+                  onClick={() => handleSmallButton(item.postId)}
+                  text="게시물 보기"
+                />
+              </SwiperSlideBox>
+            ))}
+          </Swiper>
+          {/* <Slider
             sliderData={sliderData}
             currPostIdIndex={currPostIdIndex ?? 0} // Default to 0 if null
-          />
+          /> */}
         </ContentWrapper>
       </ModalContainer>
     </>,
