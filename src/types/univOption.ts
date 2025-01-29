50f6b5bf0a5abf63680f1a192afa2157c541dc44
hiwon-lee <84234411+hiwon-lee@users.noreply.github.com>
2025-01-29 12:08:26 +0900

    🐛 fix:  update univId

--- src/types/univOption.ts
+++ src/types/univOption.ts
@@ -4,9 +4,9 @@ export interface UnivOption {
   univId: number;
 }
 export const UNIV_LIST: UnivOption[] = [
-  { value: '서강대학교', label: '서강대학교', univId: 3 },
-  { value: '연세대학교', label: '연세대학교', univId: 1 },
-  { value: '이화여자대학교', label: '이화여자대학교', univId: 2 },
+  { value: '서강대학교', label: '서강대학교', univId: 1 },
+  { value: '연세대학교', label: '연세대학교', univId: 2 },
+  { value: '이화여자대학교', label: '이화여자대학교', univId: 3 },
   { value: '홍익대학교', label: '홍익대학교', univId: 4 },
 ];
 
