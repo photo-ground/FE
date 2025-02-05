58722760c9b3979ac39b9013b4ea21fefcfe8ccb
Dahye <psst5491@naver.com>
2025-02-06 03:41:39 +0900

    🐛 fix: background 이미지 사이즈 조절

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -4,7 +4,6 @@ import styled from 'styled-components';
 const BackgroundImage = styled.img`
   position: absolute;
   width: 100%;
-  height: 100%;
   object-fit: cover;
   z-index: ${Z_INDEX.BACKGROUND_IMAGE};
 `;
