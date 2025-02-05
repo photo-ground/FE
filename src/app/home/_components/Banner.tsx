4b0ddb0d8a774c89bf065230c1c9428b23efe6dd
Dahye <psst5491@naver.com>
2025-02-05 23:42:10 +0900

    💄 ui: 배너 수정

--- src/app/home/_components/Banner.tsx
+++ src/app/home/_components/Banner.tsx
@@ -1,47 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export interface BannerProps {
-  /** How large should the Banner be? */
-  size?: 'small' | 'medium' | 'large';
-  /** Optional click handler */
-  onClick?: () => void;
-  /** Children content or fallback for the banner */
-  children?: React.ReactNode;
-}
-
-const BannerWrapper = styled.div`
-  @media (max-width: 402px) {
-    background-image: url('/images/banner~402.svg');
-    min-height: 120px;
-  }
-  @media (min-width: 403px) {
-    background-image: url('/images/banner403~520.svg');
-    min-height: 140px;
-  }
-  display: flex;
-  flex-direction: column;
-  cursor: pointer;
-  background-size: contain;
-  background-repeat: no-repeat;
-  background-position: center;
-  width: 100%;
-`;
-
-const BannerContent = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
+const ResponsiveImage = styled.img`
   width: 100%;
   height: auto;
-  padding: 1rem;
 `;
 
-export default function Banner({ size, onClick, children }: BannerProps) {
+export default function Banner() {
   return (
-    <BannerWrapper onClick={onClick} className={`Banner ${size}`}>
-      {/* <BannerImage src="/images/banner403~520.jpg" /> */}
-      {children && <BannerContent>{children}</BannerContent>}
-    </BannerWrapper>
+    <ResponsiveImage
+      src="/images/bannerSmall.webp"
+      srcSet="/images/bannerSmall.webp 402w, 
+              /images/bannerLarge.webp 520w"
+      alt="Banner"
+    />
   );
 }
