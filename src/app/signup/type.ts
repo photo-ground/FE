6fdd7dc0dd2a2da437ac9ca874b6496ca7669d0c
Dahye <psst5491@naver.com>
2025-02-07 18:43:44 +0900

    ♻️ refactor: 타입 적용

--- src/app/signup/type.ts
+++ src/app/signup/type.ts
@@ -1,15 +1,13 @@
+import { Gender } from '@/types/gender';
+import { University } from '@/types/university';
+
 export interface SignUpData {
   email: string;
   password: string;
   name: string;
   phone: string;
-  myUniv?:
-    | '서강대학교'
-    | '연세대학교'
-    | '이화여자대학교'
-    | '홍익대학교'
-    | '선택안함';
-  gender?: 'FEMALE' | 'MALE';
+  myUniv?: University | '선택안함';
+  gender?: Gender;
 
   isEmailConfirmed: boolean;
   isPasswordValid: boolean;
