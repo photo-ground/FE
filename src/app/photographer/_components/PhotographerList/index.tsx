77cd51b52b06a5fba3be50277946dfb7bccbf6f6
Dahye <psst5491@naver.com>
2025-02-25 19:54:44 +0900

    🚑 hotfix: masonry 터지는 문제 수정

--- src/app/photographer/_components/PhotographerList/index.tsx
+++ src/app/photographer/_components/PhotographerList/index.tsx
@@ -27,12 +27,16 @@ function PhotographerList({
 }) {
   useEffect(() => {
     function masonryLayout() {
-      document.querySelectorAll('.masonry-card').forEach((element) => {
-        const card = element as HTMLElement;
-        const content = card.querySelector('.masonry-card div') as HTMLElement;
-        if (content) {
-          card.style.gridRowEnd = `span ${Math.ceil(content.scrollHeight / 16 + 10 / 16)}`;
-        }
+      setTimeout(() => {
+        document.querySelectorAll('.masonry-card').forEach((element) => {
+          const card = element as HTMLElement;
+          const content = card.querySelector(
+            '.masonry-card div',
+          ) as HTMLElement;
+          if (content) {
+            card.style.gridRowEnd = `span ${Math.ceil(content.scrollHeight / 16 + 10 / 16)}`;
+          }
+        });
       });
     }
 
