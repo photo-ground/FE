2a44358f64589e77c5cd6089bcbbfdc1c143b38a
hiwon-lee <84234411+hiwon-lee@users.noreply.github.com>
2025-03-02 15:32:26 +0900

    💄 ui:  update atom small button design

--- src/components/atoms/SmallButton/Brand.tsx
+++ src/components/atoms/SmallButton/Brand.tsx
@@ -2,14 +2,14 @@ import styled from 'styled-components';
 import Text from '@/components/atoms/Text';
 import Button from './styles';
 
-const TertiaryButton = styled(Button)`
-  background: ${({ theme }) => theme.colors.gray[900]};
+const BrandButton = styled(Button)`
+  background: transparent;
+  border: 1px solid ${({ theme }) => theme.colors.primary[500]};
   cursor: pointer;
 `;
 
 const ButtonText = styled(Text)`
-  color: ${({ theme }) => theme.colors.white};
-  white-space: nowrap;
+  color: ${({ theme }) => theme.colors.primary[500]};
 `;
 
 export default function Tertiary({
@@ -20,8 +20,8 @@ export default function Tertiary({
   onClick?: () => void;
 }) {
   return (
-    <TertiaryButton onClick={onClick}>
+    <BrandButton onClick={onClick}>
       <ButtonText variant="body1_md">{text}</ButtonText>
-    </TertiaryButton>
+    </BrandButton>
   );
 }
