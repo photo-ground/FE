a6678f45c68e478493aa437a21b10eac328341f6
hiwon-lee <84234411+hiwon-lee@users.noreply.github.com>
2025-03-03 16:46:42 +0900

    💄 ui:  link underline offset design

--- src/app/signin/Form.tsx
+++ src/app/signin/Form.tsx
@@ -33,6 +33,7 @@ const Wrapper = styled.div`
 const StyledLink = styled(Link)`
   width: fit-content;
   text-decoration-color: ${({ theme }) => theme.colors.gray[300]};
+  text-underline-offset: 4px;
 `;
 const SignUpText = styled(Text)`
   color: ${({ theme }) => theme.colors.gray[300]};
