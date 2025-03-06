d1c629daa7b95b0c243202485b4337635f3685e2
hiwon-lee <84234411+hiwon-lee@users.noreply.github.com>
2025-03-06 22:46:02 +0900

    🐛 fix: ver.ios control viewport vh->dvh

--- src/app/map/style.tsx
+++ src/app/map/style.tsx
@@ -4,11 +4,13 @@ import { Z_INDEX } from '@/constants';
 export const Container = styled.div`
   position: relative;
   width: 100%;
-  height: calc(100vh - 76px);
+  height: calc(100dvh - 76px); // 와 vh -> dvh하니까 됨..ㅜㅜㅜ
+  // top: 0;
   -ms-overflow-style: none;
   ::-webkit-scrollbar {
     display: none;
   }
+  // height: calc(100dvh - env(safe-area-inset-bottom));
 `;
 
 // 칩 버튼들을 담을 컨테이너 스타일
